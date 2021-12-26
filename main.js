11ba0c431a3bce2fcbf9a8c81b76781680cbfa11
Yasaman K <yasaman@m.lo.agency>
2021-12-26 13:44:53 +0330

    feat: 6

--- main.js
+++ main.js
@@ -45,10 +45,15 @@ const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 scene.background = spaceTexture
 
 //moon
+
 const moonTexture = new THREE.TextureLoader().load('moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(3,32,32),
-    new THREE.MeshStandardMaterial({map:moonTexture,})
+    new THREE.MeshStandardMaterial({map:moonTexture,
+    normalMap:normalTexture
+    })
 )
 
 scene.add(moon);
