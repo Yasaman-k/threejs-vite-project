1171f23376a9122d598e2d56b03b1057f168fd22
Yasaman K <yasaman@m.lo.agency>
2021-12-26 13:09:29 +0330

    feat: stars

--- main.js
+++ main.js
@@ -25,11 +25,21 @@ const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 5)
+const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera,renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+    const star = new THREE.Mesh(geometry, material)
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+    star.position.set(x,y,z)
+    scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
 
 function animate() {
     requestAnimationFrame(animate)
