4d1fb0aa8d1f65841cc48c97313f9282188e944d
Yasaman K <yasaman@m.lo.agency>
2021-12-26 12:51:39 +0330

    feat: 2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { PointLight } from 'three';
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,10 +13,15 @@ camera.position.setZ(30);
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
 const torus = new THREE.Mesh(geometry, material)
 
 scene.add(torus)
+
+const pointLight =new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+scene.add(pointLight)
 
 function animate() {
     requestAnimationFrame(animate)
