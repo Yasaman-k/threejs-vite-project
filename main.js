4f5e6a38573f08bcbb772761cf3166a2c33bff24
Yasaman K <yasaman@m.lo.agency>
2021-12-26 12:58:58 +0330

    feat: 5

--- main.js
+++ main.js
@@ -13,16 +13,19 @@ camera.position.setZ(30);
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material)
 
 scene.add(torus)
 
-const pointLight =new THREE.PointLight(0xffffff)
-pointLight.position.set(5,5,5)
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointLight,ambientLight)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper)
 
 function animate() {
     requestAnimationFrame(animate)
