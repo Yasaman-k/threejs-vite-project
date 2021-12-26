1b916b663e6e7bf813ab573de0541e266a8903b3
Yasaman K <yasaman@m.lo.agency>
2021-12-26 13:03:23 +0330

    feat: Orbitcontrols

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
-import { PointLight } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -28,6 +28,7 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 5)
 scene.add(lightHelper, gridHelper)
 
+const controls = new OrbitControls(camera,renderer.domElement)
 
 
 function animate() {
@@ -35,6 +36,9 @@ function animate() {
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
+
+    controls.update()
+
     renderer.render(scene, camera)
 }
 animate()
